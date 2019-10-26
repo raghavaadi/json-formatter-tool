650ff9d355b5e5a1139ea9b9ae8ffd506ed41ee2
raghavaadi <aadithyaraghav@gmail.com>
2019-10-26 19:31:07 +0530

    done

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+var methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const index = require('./routes/index.js')
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use('/',index);
+app.use( express.static( "public" ) )
+app.use(methodOverride());
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.render('error.ejs');
+})
+const port = process.env.PORT || 3004
+app.listen(port,(err)=>
+{
+if(err)
+{
+ console.log('something wrong')   
+}
+console.log(`this is running on ${port}`)
+console.log('Press Ctrl+C')
+}
+)
